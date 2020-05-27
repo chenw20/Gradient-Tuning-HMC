f68a34f7efac13293c07e44e1224d107bef2c48f
chenw20 <60941044+chenw20@users.noreply.github.com>
2020-05-27 23:12:49 +0800

    Update mlp.py

--- decoder/mlp.py
+++ decoder/mlp.py
@@ -3,10 +3,11 @@ import tensorflow.contrib.slim as slim
 
 
 def mlp_layer(d_in, d_out, activation, name):
-    W = slim.variable(shape=(d_in, d_out), initializer=tf.contrib.layers.xavier_initializer(), name=name + '_W')
-    b = slim.variable(shape=(1, d_out), initializer=tf.zeros_initializer, name=name + '_b')
+    with tf.variable_scope(name, reuse=tf.AUTO_REUSE) as scope:
+        W = slim.variable(shape=(d_in, d_out), initializer=tf.contrib.layers.xavier_initializer(), name=name + '_W')
+        b = slim.variable(shape=(1, d_out), initializer=tf.zeros_initializer, name=name + '_b')
 
-    def apply_layer(x):
+    def apply_layer_train(x):
         a = tf.matmul(x, W) + b
         if activation == 'relu':
             return tf.nn.relu(a)
@@ -16,6 +17,16 @@ def mlp_layer(d_in, d_out, activation, name):
             return tf.nn.sigmoid(a)
         if activation == 'linear':
             return a
+    
+    def apply_layer_not_train(x):
+        a = tf.matmul(x, tf.stop_gradient(W)) + tf.stop_gradient(b)
+        if activation == 'relu':
+            return tf.nn.relu(a)
+        if activation == 'softplus':
+            return tf.nn.softplus(a)
+        if activation == 'sigmoid':
+            return tf.nn.sigmoid(a)
+        if activation == 'linear':
+            return a
 
-    return apply_layer
-
+    return apply_layer_train, apply_layer_not_train
