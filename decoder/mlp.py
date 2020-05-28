cfd94dd6f3dbbcd368c66f3d2252b15dbc76da2f
chenw20 <60941044+chenw20@users.noreply.github.com>
2020-05-29 00:50:34 +0800

    Update mlp.py

--- decoder/mlp.py
+++ decoder/mlp.py
@@ -3,9 +3,12 @@ import tensorflow.contrib.slim as slim
 
 
 def mlp_layer(d_in, d_out, activation, name):
-    #with tf.variable_scope(name, reuse=tf.AUTO_REUSE) as scope:
-    W = slim.variable(shape=(d_in, d_out), initializer=tf.contrib.layers.xavier_initializer(), name=name + '_W')
-    b = slim.variable(shape=(1, d_out), initializer=tf.zeros_initializer, name=name + '_b')
+    #with tf.variable_scope(name):
+        #W = slim.variable(shape=(d_in, d_out), initializer=tf.contrib.layers.xavier_initializer(), name=name + '_W')
+        #b = slim.variable(shape=(1, d_out), initializer=tf.zeros_initializer, name=name + '_b')
+    W = tf.get_variable(shape=(d_in, d_out), initializer=tf.contrib.layers.xavier_initializer(), name=name + '_W')
+    b = tf.get_variable(shape=(1, d_out), initializer=tf.zeros_initializer, name=name + '_b')
+
 
     def apply_layer_train(x):
         a = tf.matmul(x, W) + b
